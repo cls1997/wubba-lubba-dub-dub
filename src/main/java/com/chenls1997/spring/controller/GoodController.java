ff0be72960b036881737eb416d3aba30f2200c35
chenls <chenls1997@gmail.com>
2016-11-25 20:23:20 +0800

    2016年11月25日 20:22:43

--- src/main/java/com/chenls1997/spring/controller/GoodController.java
+++ src/main/java/com/chenls1997/spring/controller/GoodController.java
@@ -1,22 +1,24 @@
 package com.chenls1997.spring.controller;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.chenls1997.spring.model.Good;
 import com.chenls1997.spring.model.Types;
 import com.chenls1997.spring.service.GoodService;
 import com.chenls1997.spring.service.TypeService;
 import com.chenls1997.spring.util.StringUtil;
+import com.sun.corba.se.impl.encoding.CodeSetConversion;
 import com.zlzkj.core.base.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Created by Chenls on 16/11/25.
- */
+
 @Controller
 @RequestMapping(value = "good")
 public class GoodController extends BaseController {
@@ -26,8 +28,8 @@ public class GoodController extends BaseController {
     @Autowired
     private TypeService typeService;
 
-    @RequestMapping(value = "add",method = RequestMethod.POST)
-    public String addMethod(Good entity,HttpServletRequest request, HttpServletResponse response){
+    @RequestMapping(value = "good_add",method = RequestMethod.POST)
+    public String addGood(Good entity,HttpServletRequest request, HttpServletResponse response){
         Long ret = null;
         Long tid = entity.getGtypeid();
         Types t = typeService.findByID(tid);
@@ -44,8 +46,9 @@ public class GoodController extends BaseController {
             return ajaxReturn(response,null,"添加失败",0);
         }
     }
-    @RequestMapping(value = "delete",method = RequestMethod.POST)
-    public String deleteMethod(HttpServletRequest request,HttpServletResponse response){
+
+    @RequestMapping(value = "good_delete",method = RequestMethod.POST)
+    public String deleteGood(HttpServletRequest request,HttpServletResponse response){
         String delIds = request.getParameter("ids");
         String ids[] = delIds.split(",");
         Integer count = 0;
@@ -58,4 +61,58 @@ public class GoodController extends BaseController {
             return ajaxReturn(response, count+"", "删除成功", 1);
         }
     }
+
+    @RequestMapping(value = "good_edit")
+    public String editGood(Model model, HttpServletRequest request, HttpServletResponse response, Good entity){
+        if (request.getMethod().equals("POST")){
+            Integer ret=null;
+            try{
+                ret = goodService.update(entity);
+            } catch (Exception e){
+                return ajaxReturn(response,null,e.getLocalizedMessage(),-1);
+            }
+            if(ret==null){
+                return ajaxReturn(response,null,"更新失败",0);
+            } else {
+                return ajaxReturn(response, null, "更新成功", 1);
+            }
+        } else {
+            model.addAttribute("admin", goodService.findByID(entity.getGid()));
+            return "good/edit";
+        }
+    }
+
+    @RequestMapping(value = "good_view")
+    public String goodView(Model model,HttpServletRequest request,HttpServletResponse response,Good entity){
+        //// TODO: 16/11/25
+        return null;
+    }
+
+    @RequestMapping(value = "type_add")
+    public String addType(Types entity,HttpServletRequest request,HttpServletResponse response){
+        Long ret = null;
+        try {
+            ret = typeService.save(entity);
+        } catch (Exception e) {
+            ajaxReturn(response,null,e.getLocalizedMessage(),-1);
+        }
+        if (ret == null){
+            return ajaxReturn(response,null,"添加失败",0);
+        } else {
+            return ajaxReturn(response,null,"添加成功",1);
+        }
+    }
+
+    @RequestMapping(value = "type_delete")
+    public String deleteType(){
+        //TODO
+        return null;
+    }
+
+    @RequestMapping(value = "type_edit")
+    public String editType() {
+        //TODO
+        return null;
+    }
+
 }
