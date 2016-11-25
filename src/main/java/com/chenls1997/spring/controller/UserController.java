14b334c4cd5c9583d666bd1fd43166ab124b0acd
chenls <chenls1997@gmail.com>
2016-11-25 14:52:32 +0800

    UserController

--- src/main/java/com/chenls1997/spring/controller/UserController.java
+++ src/main/java/com/chenls1997/spring/controller/UserController.java
@@ -1,18 +1,83 @@
 package com.chenls1997.spring.controller;
 
+        import com.chenls1997.spring.model.User;
         import com.chenls1997.spring.service.UserService;
+        import com.chenls1997.spring.util.StringUtil;
+        import com.zlzkj.core.base.BaseController;
         import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.beans.factory.annotation.Value;
         import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.RequestBody;
         import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Chenls on 16/11/23.
  */
 @Controller
 @RequestMapping(value = "user")
-public class UserController {
+public class UserController extends BaseController {
     @Autowired
-    private UserService service;
+    private UserService userService;
 
-    //TODO
+    @RequestMapping(value = "register")
+    public String registerHandler(User entity, HttpServletRequest request, HttpServletResponse response){
+        if (request.getMethod().equals("POST")) {
+            Integer status=null;
+            try{
+                status = userService.save(entity);
+            }catch (Exception e){
+                return ajaxReturn(response,null,e.getLocalizedMessage(),-1);
+            }
+            if(status==0){
+                return ajaxReturn(response,null,"注册失败",0);
+            }else {
+                return ajaxReturn(response,null,"注册成功",1);
+            }
+        }else{
+            return "user/register";
+        }
+    }
+
+    @RequestMapping(value = "login")
+    public String loginHandler(Model model,HttpServletRequest request,HttpServletResponse response){
+        if(request.getMethod().equals("POST")){
+            String username = (String)request.getAttribute("username");
+            String password = (String)request.getAttribute("password");
+            User entity = userService.LoginGetObj(username,password);
+            if (entity==null) {
+                return ajaxReturn(response,null,"登陆失败",0);
+            }else {
+                return ajaxReturn(response,entity,"登陆成功",1);
+            }
+        }else{
+            return "user/login";
+        }
+    }
+
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    public String deleteHandler(HttpServletRequest request,HttpServletResponse response){
+        String delIds = request.getParameter("ids");
+        String ids[] = delIds.split(",");
+        int count = 0;
+        for(String id : ids){
+            count += userService.delete(StringUtil.stringToInteger(id));
+        }
+        if(count==0){
+            return ajaxReturn(response,null,"删除失败",0);
+        } else {
+            return ajaxReturn(response,count+"","删除成功",1);
+        }
+    }
+
+    @RequestMapping(value = "edit")
+    public String userpanelHandler(Model model,HttpServletRequest request,HttpServletResponse response,User entity){
+        //TODO
+        return null;
+    }
+
 }
--- src/test/java/com/zlzkj/core/base/BaseSpringTest.java
+++ src/test/java/com/zlzkj/core/base/BaseSpringTest.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring/root-context.xml","classpath:spring/app-servlet-context.xml"})
+@ContextConfiguration({"classpath:spring/test-context.xml","classpath:spring/app-servlet-context.xml"})
 public abstract class BaseSpringTest {
 	
 	
